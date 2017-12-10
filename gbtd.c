d28ee8d6a560b7853eab03fac336bef94468e2fb
Sergei Litvinenko <sin486@gmail.com>
2017-12-10 13:24:03 -0800

    Add example sprite

--- gbtd.c
+++ gbtd.c
@@ -1,7 +1,14 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include "foo.c"
 
 void main() {
     printf("Hello World!");
     printf("\n\nPress Start");
+
+    SPRITES_8x8;
+    set_sprite_data(0, 8, foo);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 75, 75);
+    SHOW_SPRITES;
 }
