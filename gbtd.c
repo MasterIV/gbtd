256b1cea92bd7249e60b83117828459de66a88d9
Syrup <tobias@rojahn.org>
2017-12-21 09:27:56 +0100

    Flying rocket

--- gbtd.c
+++ gbtd.c
@@ -1,42 +1,43 @@
 #include <gb/gb.h>
 #include <stdio.h>
+#include <rand.h>
 
 #include "background.tiles"
+#include "objects.tiles"
 #include "lanes.map"
 
-int xpos;
-int dir;
-int i;
 
 void main() {
-    printf("Hello Roman!");
-    printf("\n\nPress Start");
-    printf("\n\n\n\n\n\n\nKilroy was here.");
-
+  	unsigned int ypos, xpos;
+  
     SPRITES_8x16;
-    set_sprite_data(0, 10, background);
   
-  	for(i =0; i < 1; i++)
-    	set_sprite_tile(i, i);
+    set_sprite_data(0, 24, objects);
+  	set_bkg_data(0, 20, background);
   
-    move_sprite(0, 75, 75);
+    VBK_REG = 1;
+	VBK_REG = 0;
   
+	set_bkg_tiles(0, 0, 17, 30, lanes);
   
-    SHOW_SPRITES;
-
-    xpos = 75; 
-    dir = 1; 
+    set_sprite_tile(0, 4);
+    move_sprite(0, 75, 160);
+  
+    SHOW_SPRITES;   
+  	SHOW_BKG;
+  	DISPLAY_ON;
+  
+    ypos = 160;
+  	xpos = 75;
 
     while(1) {
-        move_sprite(0, xpos, 75);
-  		set_sprite_prop(0, xpos > 75 ? S_PALETTE : 0 );
+        move_sprite(0, xpos, ypos -= 1);
 
-        if(dir==1) {
-            xpos += 1;
-            if(xpos>110) dir = 0;
-        } else {
-            xpos -= 1;
-            if(xpos<40) dir = 1;
+        if(ypos < 1) {
+		  	xpos = rand();
+		  	if( xpos < 16 ) xpos = 16;
+		  	if( xpos > 120 ) xpos = 120;
+    		ypos = 160;
         }
 
         delay(10);
